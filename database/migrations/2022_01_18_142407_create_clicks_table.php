99b6fcb822dcb7cf5d30bc638bb57d9aabe4c5f0
ZAHIR SOFFI <soffizahir2016@gmail.com>
2022-01-19 15:21:13 +0100

    Building a job board in Laravel [Pt. 1] - Models + Migrations

---
+++ database/migrations/2022_01_18_142407_create_clicks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClicksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clicks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Listing::class);
+            $table->text('user_agent')->nullable();
+            $table->text('ip')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clicks');
+    }
+}
---
+++ database/migrations/2022_01_18_142644_create_listing_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Listing;
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListingTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listing_tag', function (Blueprint $table) {
+            $table->foreignIdFor(Listing::class);
+            $table->foreignIdFor(Tag::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listing_tag');
+    }
+}
